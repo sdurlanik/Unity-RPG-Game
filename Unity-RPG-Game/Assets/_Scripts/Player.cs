7d79ffe860f0af1d4356488d17959eba1ee1faa0
Samed Durlanık <sametdurlanik@gmail.com>
2022-01-18 14:24:35 +0300

    Add player attack ability
    Created structure for attack ability, adding attack animations for 2 sides (left and right)

---
+++ Unity-RPG-Game/Assets/_Scripts/Player.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField]private Stat health;
+    [SerializeField]private Stat mana;
+    [SerializeField] private float InitHealth = 100;
+    [SerializeField] private float InitMana = 50;
+    
+    private SpriteRenderer playerSpriteRenderer;
+    private bool isFlipped = false;
+
+    [SerializeField] private GameObject leftSpear;
+    [SerializeField] private GameObject rightSpear;
+
+   
+    
+
+    protected override void Start()
+    {
+        health.Initialize(InitHealth,InitHealth);
+        mana.Initialize(InitMana,InitMana);
+
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        
+        
+        
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        GetInput();
+        SetSpearPosition();
+        // Character.cs scripti içerisindeki Update() fonksiyonunda bulunan komutları çalıştırır
+        base.Update();
+    }
+
+    
+
+    // Klavyeden girilen WASD değerlerini tespit eder ve Vector2 türündeki
+    // direction değişkenine bu değeri aktarır. Klavyeden parmağımızı çektiğimizde
+    // değer sıfırlanır (Vector2.zero)
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+
+            if (isFlipped && !myAnimator.GetBool("rightAttack"))
+            {
+                playerSpriteRenderer.flipX = false;
+                isFlipped = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+            if (!isFlipped && !myAnimator.GetBool("leftAttack"))
+            {
+                playerSpriteRenderer.flipX = true;
+                isFlipped = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnimator.SetBool("isAttacking", true);
+            StartCoroutine(StartAttack());
+        }
+    }
+
+    // Karakter saldırı fonksiyonu
+    public IEnumerator StartAttack()
+    {
+        myAnimator.SetBool("isAttacking",true);
+        
+        if (isFlipped)
+        {
+            myAnimator.SetBool("rightAttack",true);
+            myAnimator.SetBool("leftAttack", false);
+        }
+        else
+        {
+            myAnimator.SetBool("leftAttack", true);
+            myAnimator.SetBool("rightAttack",false);
+
+        }
+        
+        
+        yield return new WaitForSeconds(1); // Debug için hardcode (Daha sonra değiştirilecek)
+        
+        myAnimator.SetBool("leftAttack", false);
+        myAnimator.SetBool("rightAttack", false);
+        myAnimator.SetBool("isAttacking", false);
+    }
+
+    private void SetSpearPosition()
+    {
+        if (isFlipped)
+        {
+            leftSpear.SetActive(false);
+            rightSpear.SetActive(true);
+        }
+        else
+        {
+            leftSpear.SetActive(true);
+            rightSpear.SetActive(false);
+        }
+    }
+}
---
+++ Unity-RPG-Game/Assets/_Scripts/Stat.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour
+{
+   private Image content;
+   [SerializeField] private float lerpSpeed;
+   private float currentFill;
+   public float MyMaxValue { get; set; }
+   private float currentValue;
+
+   [SerializeField]
+   private Text statValue;
+
+   public float MyCurrentValue
+   {
+      get
+      {
+         return currentValue;
+      }
+      set
+      {
+         // Can ve mana değerlerini tanımlar (100den büyük 0dan küçük olamaz)
+         if (value > MyMaxValue)
+         {
+            currentValue = MyMaxValue;
+         }
+         else if (value < MyMaxValue)
+         {
+            currentValue = 0;
+         }
+         else
+         {
+            currentValue = value;
+         }
+
+         currentFill = currentFill / MyMaxValue;
+         statValue.text = currentValue + "/" + MyMaxValue;
+      }
+   }
+
+   private void Start()
+   {
+      content = GetComponent<Image>();
+   }
+
+   private void Update()
+   {
+ 
+      
+   }
+
+
+   public void Initialize(float currentValue, float maxValue)
+   {
+      MyMaxValue = maxValue;
+      MyCurrentValue = currentValue;
+   }
+   
+}
