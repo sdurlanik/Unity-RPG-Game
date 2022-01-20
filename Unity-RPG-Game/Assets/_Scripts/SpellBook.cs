91a12c7e794402ebe49075a159d401f33a51dcb4
Enes <enes.uca@vega.com.tr>
2022-01-20 16:56:52 +0300

    Completed castingbar mechanics

--- Unity-RPG-Game/Assets/_Scripts/SpellBook.cs
+++ Unity-RPG-Game/Assets/_Scripts/SpellBook.cs
@@ -2,10 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellBook : MonoBehaviour
 {
 
+    [SerializeField] private Image castingBar;
+    [SerializeField] private Text spellName;
+    [SerializeField] private Image icon;
+    [SerializeField] private Text castTime;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    private Coroutine spellRoutine;
+    private Coroutine fadeRoutine;
+    
     [SerializeField] private Spell[] spells;
     void Start()
     {
@@ -20,6 +30,77 @@ public class SpellBook : MonoBehaviour
 
     public Spell CastSpell(int index)
     {
+        castingBar.fillAmount = 0;
+        
+        castingBar.color = spells[index].MyBarColor;
+
+        spellName.text = spells[index].MyName;
+
+        icon.sprite = spells[index].MyIcon;
+
+        spellRoutine = StartCoroutine(Progress(index));
+
+        fadeRoutine = StartCoroutine(FadeBar());
+
         return spells[index];
+    }
+
+    private IEnumerator Progress(int index)
+    {
+        float timePassed = Time.deltaTime;
+
+        float rate = 1.0f / spells[index].MyCastTime;
+
+        float progress = 0.0f;
+
+        while (progress <= 1)
+        {
+            castingBar.fillAmount = Mathf.Lerp(0, 1, progress);
+            progress += rate * Time.deltaTime;
+
+            timePassed += Time.deltaTime;
+            castTime.text = (spells[index].MyCastTime - timePassed).ToString("F2");
+
+            if (spells[index].MyCastTime - timePassed < 0)
+            {
+                castTime.text = "0.00";
+            }
+            
+            yield return null;
+        }
+        
+        StopCasting();
+    }
+
+    private IEnumerator FadeBar()
+    {
+
+        float rate = 1.0f / 0.5f;
+
+        float progress = 0.0f;
+
+        while (progress <= 1)
+        {
+            
+            canvasGroup.alpha = Mathf.Lerp(0, 1, progress);
+            progress += rate * Time.deltaTime;
+
+            yield return null;
+        }
+    }
+    
+    public void StopCasting()
+    {
+        if (fadeRoutine != null)
+        {
+            StopCoroutine(fadeRoutine);
+            canvasGroup.alpha = 0;
+            fadeRoutine = null;
+        }
+        if (spellRoutine != null)
+        {
+            StopCoroutine(spellRoutine);
+            spellRoutine = null;
+        }
     }
 }
